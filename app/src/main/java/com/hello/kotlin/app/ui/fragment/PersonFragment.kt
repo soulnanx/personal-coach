3c3772f2b87cf6695614fa956bb5761beeff9a97
renan <rsantos.dev@gmail.com>
2016-06-29 23:37:57 -0300

    fragment person

--- app/src/main/java/com/hello/kotlin/app/ui/fragment/PersonFragment.kt
+++ app/src/main/java/com/hello/kotlin/app/ui/fragment/PersonFragment.kt
@@ -3,7 +3,6 @@ package com.hello.kotlin.app.ui.fragment
 import android.app.ProgressDialog
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
